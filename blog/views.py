01166deca3c8e27d7f804f982da6b8fc6270f090
Jenny Liu <jenny.liu.ca@gmail.com>
2023-01-30 19:58:51 -0800

    add blogs blog item

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Blog
 
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    blogs = Blog.objects.all()
+    return render(request, 'blog/all_blogs.html', {'blogs': blogs})
